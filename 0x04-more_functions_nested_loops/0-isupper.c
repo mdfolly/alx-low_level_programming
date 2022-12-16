095543fc220b6515af1cd40d2c1de3137fb2a7c1
mdfolly <deecoder@gmail.com>
2022-12-16 12:28:54 -0800

    alx

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,44 +3,43 @@
 
 
 /**
- *
- *  * _isupper - A function that checks for uppercase character.
- *
- *   * @c: An input character
- *
- *    * Return: 1 if c is uppercase or 0 otherwise
- *
- *     */
+
+ * _isupper - A function that checks for uppercase character.
+
+ * @c: An input character
+
+ * Return: 1 if c is uppercase or 0 otherwise
+
+ */
 
 int _isupper(int c)
 
 {
 
-		char uppercase = 'A';
+	char uppercase = 'A';
 
-			int isupper = 0;
-
-
-
-				for (; uppercase <= 'Z'; uppercase++)
-
-						{
-
-									if (c == uppercase)
-
-												{
-
-																isupper = 1;
-
-																			break;
-
-																					}
-
-										}
+	int isupper = 0;
 
 
 
-					return (isupper);
+	for (; uppercase <= 'Z'; uppercase++)
+
+	{
+
+		if (c == uppercase)
+
+		{
+
+			isupper = 1;
+
+			break;
+
+		}
+
+	}
+
+
+
+	return (isupper);
 
 }
-
