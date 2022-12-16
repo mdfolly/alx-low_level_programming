6639cea7e6391bad7933c1933eebeda63aedfe3b
mdfolly <deecoder@gmail.com>
2022-12-16 06:53:46 -0800

    upper case

---
+++ isupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isupper - uppercase letters
+ *
+ *   * @c: character to check
+ *
+ *    * Return: 1 if it is in uppercase character, 0 in other case.
+ *
+ *     */
+
+int _isupper(int c)
+
+{
+
+		if (c >= 65 && c <= 90)
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
+}
